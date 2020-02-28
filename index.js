206c0f42ac5b33dc42d17e22043f4ec79218928a
Justin Evans <evansjustindavid@gmail.com>
2020-02-28 20:44:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,6 @@ var object = {
 
 }
 
-
-min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-
-
-
 function viewCart() {
   // write your code here
 }
