5223e43b227fd94ddfbac9aa4c9405d8e438c0a7
Justin Evans <evansjustindavid@gmail.com>
2020-02-28 20:54:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(item) {
 
 var object = {
   itemName: `${item}`,
-  itemPrice: `${parseInt(Math.random() * 100, 10)}`
+  itemPrice: `${100 * parseInt((Math.random() * 100), 10)}`
 }
  cart.push(object);
  return `${item} has been added to your cart.` 
