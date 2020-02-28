4ea34abc25091b19a38dc9c28cb4e70d66ac984d
Justin Evans <evansjustindavid@gmail.com>
2020-02-28 20:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ item.itemName = `${item}`;
+ item.itemPrice = `${(Math.random() * Math.floor(1000))}`; 
+ 
+ cart.push(itemName, itemPrice)
+ 
 }
 
 function viewCart() {
